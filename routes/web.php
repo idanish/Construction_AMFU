1faa078dfdcc222c0c8fcf11ff18558e0577759d
ceorehan <rehan2407f@aptechorangi.com>
2025-09-08 20:41:03 +0500

    rehan

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViewController;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HomeControlle;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\UserManagementController;
 use App\Http\Controllers\SettingsController;
